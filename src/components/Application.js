5b5f39c4f827e49282df45cc62f53bd2c0d9145d
Manali7494 <manalibhattacharyya10@gmail.com>
2020-06-17 22:45:04 -0600

    Replace mock appointments with live data

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,69 +4,7 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import axios from "axios";
-
-const appointmentsList = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id:3,
-    time: "5pm",
-    interview: {
-      student: "Student 2",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id:4,
-    time: "3pm",
-    interview: {
-      student: "Student 3",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id:3,
-    time: "2pm",
-    interview: {
-      student: "Student 3",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
-
-const appointments = appointmentsList.map(
-  appointment => {
-    return (
-      <Appointment key={appointment.id} {...appointment} />
-    );
-  }
-);
+import {getAppointmentsForDay} from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -78,15 +16,23 @@ export default function Application(props) {
   const setDay = day => setState(state => ({ ...state, day }));
 
   useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      setState(state => ({ ...state, days: response.data }));
-    })
-    .catch((error) => {
-      console.log(error.response.status);
-      console.log(error.response.headers);
-      console.log(error.response.data);
-    })
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+    ]).then(
+      (all) => 
+        setState(state => ({ ...state, days: all[0].data, appointments: all[1].data }))
+    );
   }, []);
+
+  const appointments = getAppointmentsForDay(state, state.day).map(
+    appointment => {
+      return (
+        <Appointment key={appointment.id} {...appointment} />
+      );
+    }
+  );
+  
 
   return (
     <main className="layout">
